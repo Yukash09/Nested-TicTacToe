9e4cce0e2c29e10d7a7a9606fe8848136f4c6a7e
Yukash09 <yukayugan@gmail.com>
2024-09-28 12:01:52 +0530

    Added comments

--- classes.js
+++ classes.js
@@ -1,10 +1,33 @@
+/* Structure of the Game:
+  Ultraboard : Consists of 9 'Board's (placed in the array 'boards')
+  Board: Consists of 9 'cell's (placed in the array 'cells')
+  Cell: Has a state , active , play() function that changes the state of the cell, when clicked.
+       If state = 0 : Unoccupied 
+          state = 1 : Occupied by x
+          state = 2 : Occupied by o
+       If active = true - Can be played
+          active = false - Cannot be played
+  Similarly for each board there is a state and active
+  Additionally , for a Board:
+      activate() function - activates/deactivates the boards and available cells for a particular turn
+      checkboard() function - checks if a board has reached a win condition (denoted by winning combinations)
+      Need to implement checkdraw() function (making a new state = 2)
+  And for the UltraBoard:
+      checkstate() function - checks the win condition over multiple boards 
+      checkdraw() function - checks if there is a draw (Need rework by making new state and add corresponding changes)
+      checkwinner() function - returns the winner (if the game ends)
+      start() - to setup the board properly (not used for now)
+ */
+
 class Player {
+
   constructor(string) {
     this.name = string;
     this.wins = 0;
     this.losses = 0;
     this.ties = 0;
   }
+
   result(val) {
     if (val == 1) {
       this.wins += 1;
@@ -14,33 +37,43 @@ class Player {
       this.losses += 1;
     }
   }
+  // To-do : Implement other functions and statistics 
 }
 
+
 class cell {
+
   constructor(x, y, r, c) {
-    this.state = 0;
+    this.state = 0; 
     this.active = false;
     this.boardrow = x;
     this.boardcol = y;
     this.row = r;
     this.col = c;
   }
+  
+  // Initialization 
   init() {
     this.state = 0;
     this.active = false;
   }
 
+  // Play
   play(val) {
     this.state = val;
   }
+
+  // Debug
   print(){
     console.log("State of cell " , this.row , this.col , " :" , this.state);
   }
 }
 
 class Board {
+
   constructor(x, y) {
     this.cells = [];
+
     for (let i = 0; i < 3; ++i) {
       let dum = [];
       for (let j = 0; j < 3; ++j) {
@@ -48,10 +81,12 @@ class Board {
       }
       this.cells.push(dum);
     }
+
     this.state = 0;
     this.row = x;
     this.col = y;
     this.active = false;
+
     this.winconditions = [
       [0, 1, 2],
       [3, 4, 5],
@@ -63,6 +98,8 @@ class Board {
       [2, 4, 6],
     ];
   }
+
+  // Initialization
   init() {
     this.state = 0;
     this.active = false;
@@ -72,21 +109,26 @@ class Board {
       }
     }
   }
+
+  // Play
   play(r, c, val) {
     this.cells[i][j].play(val);
   }
 
   checkboard(val) {
+
     for (let i = 0; i < 8; i++) {
       let check1 = this.winconditions[i][0];
       let check2 = this.winconditions[i][1];
       let check3 = this.winconditions[i][2];
+
       let mod1 = check1%3 ;
       let quo1 = floor(check1/3);
       let mod2 = check2%3 ;
       let quo2 = floor(check2/3);
       let mod3 = check3%3 ;
       let quo3 = floor(check3/3);
+
       if (
         this.cells[mod1][quo1].state == this.cells[mod2][quo2].state &&
         this.cells[mod2][quo2].state == this.cells[mod3][quo3].state &&
@@ -99,8 +141,10 @@ class Board {
     }
     return false;
   }
+
   activate(bool) {
     this.active = bool;
+
     for (let i = 0; i < 3; ++i) {
       for (let j = 0; j < 3; ++j) {
         if (this.cells[i][j].state == 0 && bool == true) {
@@ -111,12 +155,15 @@ class Board {
       }
     }
   }
+  
+  // Debug
   print(){
     console.log("State of board " , this.row , this.col , " :" , this.state);
   }
 }
 
 class UltraBoard {
+
   constructor() {
     this.boards = [];
     for (let i = 0; i < 3; ++i) {
@@ -139,16 +186,21 @@ class UltraBoard {
       [2, 4, 6],
     ];
   }
+
   init() {
     this.state = 0;
     this.active = false;
   }
+
   play(x, y, r, c, val) {
     this.boards[x][y].play(r, c, val);
   }
+
+  // Debug
   print(){
     console.log("State of full:" , this.state);
   }
+
   checkstate(val) {
     for (let i = 0; i < 8; i++) {
       let check1 = this.winconditions[i][0];
@@ -170,6 +222,7 @@ class UltraBoard {
       }
     }
   }
+  
   checkdraw(){
     for(let i = 0 ; i < 3 ; ++i){
         for(let j = 0 ; j < 3 ; ++j){
@@ -180,6 +233,7 @@ class UltraBoard {
     }
     return true ;
   }
+
   checkwinner() {
     if (this.state == 1) {
         return [true , 1] ;
--- sketch.js
+++ sketch.js
@@ -1,19 +1,41 @@
+// Imports classes.js (Header file)
+
 function setup() {
-  createCanvas(600, 800);
+  createCanvas(1525, 800);
+}
+
+/* Try to use Images instead of x and o?? */
+// let toothless ;
+// let pikachu ;
+
+let font ;
+
+function preload(){
+  // toothless = loadImage('./assets/toothless.png');
+  // pikachu = loadImage('./assets/pika.png') ;
+
+  font = loadFont('./assets/lemon-jelly-font/LemonJellyPersonalUse-dEqR.ttf')
 }
 
 function drawboard(Ultraboard){
+  // translate(450 , 25);  // To-do: Make the board centralized by appropriately changing coordinates
   for(let i = 0 ; i < 3 ; ++i){
     for(let j = 0 ; j < 3 ; ++j){
-      posX = (Ultraboard.boards[i][j].row+1) * 100 * 2 - 100;
+
+      // Set the drawing cursor position appropriately
+      posX = (Ultraboard.boards[i][j].row+1) * 100 * 2 - 100 ;
       posY = (Ultraboard.boards[i][j].col+1) * 100 * 2 - 100 ;
       translate(posX , posY) ;
-      let nsize = (this.width/3 - ((this.width/(3))/100*2));
+
+      // Draw squares for each board
+      let nsize = (200 - ((200/(3))/100*2));
       textAlign(CENTER);
       textSize(nsize/3);
-      stroke(200);
-      noFill();
+      stroke(300);
+      fill(0);
       square(-nsize/2  , -nsize/2 , nsize);
+
+      // Draw the inner lines for each square
       for(let k = 1 ; k <= 2 ; ++k){
         stroke(100)
         line(-nsize/2   , -nsize/2 + k*nsize/3  , nsize/2  , -nsize/2 + k*nsize/3);
@@ -21,147 +43,278 @@ function drawboard(Ultraboard){
       for(let k = 1 ; k <= 2 ; ++k){
         line(-nsize/2 + k* nsize/3  ,  -nsize/2   , -nsize/2+ k*nsize/3 , +nsize/2 );     
       }
+
+      // Draw x or o based on the change in state of each cell
       for(let k = 0 ; k < 3 ; ++k){
         for(let l = 0 ; l < 3 ; ++l){
+
           if(Ultraboard.boards[j][i].cells[k][l].state == 1){
-            stroke(255 , 0 , 0);
-            text('X' , -nsize/2 + (l*2 + 1) * nsize/6 , -nsize/2 + (k*2 + 1)* nsize/6 + nsize/9) ;
+            textFont(font);
+            stroke(255 , 0 ,  0);
+            fill(255 , 0 , 0);
+            text('x' , -nsize/2 + (l*2 + 1) * nsize/6 , -nsize/2 + (k*2 + 1)* nsize/6 + nsize/9) ;
+            // image(pikachu , -nsize/2 + (l*2 + 1)*nsize/6 , -nsize/2 + (k*2 + 1)* nsize/6 + nsize/9 )
           }
+
           else if(Ultraboard.boards[j][i].cells[k][l].state == -1){
-            stroke(0 , 255 , 0);
-            text('O' , -nsize/2 + (l*2 + 1) * nsize/6  , -nsize/2 + (k*2 + 1)* nsize/6 + nsize/9) ;
-          }  
+            textFont(font);
+            stroke(0 , 0 , 255);
+            fill(0 , 0 , 255);
+            text('o' , -nsize/2 + (l*2 + 1) * nsize/6  , -nsize/2 + (k*2 + 1)* nsize/6 + nsize/9) ;
+          }
+
+          // Debug  
           else{
             // stroke(0 , 255 , 0);
             // text('0', -nsize/2 + (l*2 + 1) * nsize/6  , -nsize/2 + (k*2 + 1)* nsize/6 + nsize/9);
           }          
         }
       } 
+
+      // If a board is completed , Fill with bigger x or o correspondingly
+      if(Ultraboard.boards[j][i].state == 1){
+
+        // Draw square
+        fill(0);
+        stroke(255);
+        square(-nsize/2  , -nsize/2 , nsize);
+
+        // Draw o
+        stroke(255 , 0 , 0) ;
+        fill(255 , 0 , 0);
+        textSize(nsize);
+        text('x' , 0 , nsize/9);
+      }
+      else if(Ultraboard.boards[j][i].state == -1){
+        
+        // Draw square
+        fill(0);
+        stroke(255);
+        square(-nsize/2  , -nsize/2 , nsize);
+
+        // Draw o
+        stroke(0 , 0 , 255) ;
+        fill(0 , 0 , 255);
+        textSize(nsize);
+        text('o' , 0 , nsize/9);        
+      }
+
+      // Show all the playable boards for each turn of the game
+
+      if(Ultraboard.boards[j][i].active){
+        noFill();
+        stroke(0 , 255 , 0);
+        square(-nsize/2 + 5  , -nsize/2 + 5 , nsize-10);
+      }
       translate(-posX , -posY) ;
     }
   }
+  // Get the size and cursor properly w.r.t the whole board
+  let nsize = (200 - ((200/(3))/100*2));
+  posX = (2) * 100 * 2 - 100;
+  posY = (2) * 100 * 2 - 100 ;
+
+  // Do the same for the whole board
+  if(Ultraboard.state == 1){
+    fill(0);
+    stroke(255 , 0 , 0);
+    square(0 , 0 , 600);
+
+    stroke(255 , 0 , 0) ;
+    fill(255 , 0 , 0);
+    translate(posX , posY) ;
+    textSize(300);
+    text('x' , 0 , nsize/3);
+    
+    // Bottom text
+    textSize(100);
+    textAlign(CENTER);
+    translate(-posX , -posY) ;
+    text("Hehehaw won" , 200 , 700);
+  }
+
+  else if(Ultraboard.state == -1){
+    fill(0);
+    stroke(0 , 0 , 255);
+    square(0 , 0 , 600);
+
+    stroke(0 , 0 , 255) ;
+    fill(0 , 0 , 255);
+    translate(posX , posY) ;
+    textSize(300);
+    text('o' , 0 , nsize/3);
+
+    // Bottom text
+    textSize(100);
+    textAlign(CENTER);
+    text("Hohohaw won" , 0 , 700);
+  }
 }
 
-
-
+// Create Player class (To-do: Add accounts system?)
 let P1 = new Player("Hehehaw") ;
 let P2 = new Player("Hohohaw") ;
-let play = true ;
+
+let play = true ; 
+
+// Setup the Ultraboard 
 let Newboard = new UltraBoard() ;
 Newboard.init();
 Newboard.start();
-let currplayer = 1 ;
 
-function getcrtboard(){
-  let r = floor((mouseY) / 200);
-  let c = floor((mouseX) / 200);
-  console.log("Coordinates of boards:" , r  , c);
+let currplayer = 1 ; // currplayer: 1 - x ; -1 - y ;
 
-  return Newboard.boards[r][c] ;
-}
-
-function getcursorsquare(){
-  let sx = floor((mouseX) / 200) * 200 ;
-  let sy = floor((mouseY) / 200) * 200 ;
-  let nx = mouseX - sx ;
-  let ny = mouseY - sy ;
-  let r = floor(ny / (200/3));
-  let c = floor(nx / (200/3));
-  console.log("Coordinates of cells:" , r , c);
-  return [r , c] ;
-
-}
-
-function mousePressed(){
-  if(mouseX > 600 || mouseY > 600){
-    return ;
+if(play){
+  // helper function to get the board where the player clicks
+  function getcrtboard(){
+    let r = floor((mouseY) / 200);
+    let c = floor((mouseX) / 200);
+    console.log("Coordinates of boards:" , r  , c);
+  
+    return Newboard.boards[r][c] ;
   }
-  else{
-    let crtboard = getcrtboard();
-    if(!crtboard.active){
+  // helper function to get the square where the player clicks
+  function getcursorsquare(){
+    let sx = floor((mouseX) / 200) * 200 ;
+    let sy = floor((mouseY) / 200) * 200 ;
+    let nx = mouseX- sx ;
+    let ny = mouseY - sy ;
+    let r = floor(ny / (200/3));
+    let c = floor(nx / (200/3));
+    console.log("Coordinates of cells:" , r , c);
+    return [r , c] ;
+  
+  }
+  // function that checks if the click is a valid and move should be made or not
+  function mousePressed(){
+
+    // Out of bound conditions or the game is over already
+    if(mouseX > 600 || mouseY > 600 || !play){
       return ;
     }
-    let crtsqr = getcursorsquare() ;
-    if(!crtboard.cells[crtsqr[0]][crtsqr[1]].active){
-      return ;
+
+    else{
+      let crtboard = getcrtboard();
+
+      // Check if the board is active
+      if(!crtboard.active){
+        return ;
+      }
+
+      let crtsqr = getcursorsquare() ;
+
+      // Check if the cell is active
+      if(!crtboard.cells[crtsqr[0]][crtsqr[1]].active){
+        return ;
+      }
+
+      // Play if both are active
+      playmove(crtboard , crtsqr , Newboard);
     }
-    playmove(crtboard , crtsqr , Newboard);
   }
-}
-function swap(){
-  console.log("Swapping turns");
-    currplayer *= -1 ;
-}
-function playmove(crtboard , crtsqr , ultra){
-  crtboard.cells[crtsqr[0]][crtsqr[1]].play(currplayer) ;
-  let prev = currplayer
-  swap();
-  if(ultra.boards[crtsqr[0]][crtsqr[1]].state != 0){
-    console.log("All boards playable state")
-    for(let i = 0 ; i < 3 ; ++i){
-      for(let j = 0 ; j < 3 ; ++j){
-        if(ultra.boards[i][j].state == 0){
-          ultra.boards[i][j].activate(true);
+
+  function swap(){
+    console.log("Swapping turns");
+    currplayer *= -1 ; // Swap player
+  }
+
+  function playmove(crtboard , crtsqr , ultra){
+
+    crtboard.cells[crtsqr[0]][crtsqr[1]].play(currplayer) ; // Play the move in the cell
+
+    let prev = currplayer // Store before swapping (Not really needed)
+    swap(); // Swap turns
+
+    // Check if the corresponding board is available
+    if(ultra.boards[crtsqr[0]][crtsqr[1]].state != 0){
+      // If not then activate all the possible boards (boards where the game isn't completed)
+      console.log("All boards playable state")
+      for(let i = 0 ; i < 3 ; ++i){
+        for(let j = 0 ; j < 3 ; ++j){
+          if(ultra.boards[i][j].state == 0){
+            ultra.boards[i][j].activate(true);
+          }
         }
       }
     }
-  }
-  else{
-    for(let i = 0 ; i < 3 ; ++i){
-      for(let j = 0 ; j < 3 ; ++j){
-        ultra.boards[i][j].activate(false);
+
+    else{
+      // Else deactivate all the other boards and activate only the corresponding board
+      for(let i = 0 ; i < 3 ; ++i){
+        for(let j = 0 ; j < 3 ; ++j){
+          ultra.boards[i][j].activate(false);
+        }
       }
+      ultra.boards[crtsqr[0]][crtsqr[1]].activate(true);
+      console.log("Playable board" , crtsqr[0] , crtsqr[1]);
     }
-    ultra.boards[crtsqr[0]][crtsqr[1]].activate(true);
-    console.log("Playable board" , crtsqr[0] , crtsqr[1]);
-  }
-  let change = crtboard.checkboard(prev) ;
-  if(change){
-    ultra.checkstate(prev);
-    let winner = ultra.checkwinner();
-    if(winner[0]){
-      play = false ;
-      endgame(winner[1]);
+
+    // Check for the change in state of the board
+    let change = crtboard.checkboard(prev) ;
+
+    if(change){
+
+      // If there is a change, then check for change in state of the Ultraboard
+      ultra.checkstate(prev);
+
+      let winner = ultra.checkwinner(); // Check if the game is finished
+
+      if(winner[0]){
+
+        // If yes, then end the game (To-do: Make a restart button which starts the game all over again)
+        play = false ;
+        // endgame(winner[1]);
+      }
+
+      // If no, continue playing
     }
+
+    // Debug
+    crtboard.print();
+    crtboard.cells[crtsqr[0]][crtsqr[1]].print();
+    ultra.print();
+    console.log("play:" , play);
   }
-  crtboard.print();
-  crtboard.cells[crtsqr[0]][crtsqr[1]].print();
-  ultra.print();
 }
 
-function endgame(winner){
-  Newboard.init();
-  background(255) ;
-  strokeWeight(2);
-  stroke(0);
-  fill(0);
-  textSize(100);
-  textAlign(CENTER);
-  if(winner == 1){
-    text("Hehehaw won" , 300 , 250);
-  }
-  else if(winner == -1){
-    text("Hohohaw won" , 300 , 250);
-  }
-  else{
-    text("Drew" , 300 , 250);
-  }
-  textSize(20);
-  noStroke();
-  text("Click to restart", 300 , 350);
-  setTimeout(function(){
-    play = true ;
-  }, 1000);
-}
+// End game implementation failed terribly (Need to fix, need to add restart game)
+// function endgame(winner){
+//   // Newboard.init();
+//   background(255) ;
+//   strokeWeight(2);
+//   stroke(0);
+//   fill(0);
+//   textSize(100);
+//   textAlign(CENTER);
+//   if(winner == 1){
+//     text("Hehehaw won" , 300 , 250);
+//   }
+//   else if(winner == -1){
+//     text("Hohohaw won" , 300 , 250);
+//   }
+//   else{
+//     text("Drew" , 300 , 250);
+//   }
+//   textSize(20);
+//   noStroke();
+//   text("Click to restart", 300 , 350);
+//   // setTimeout(function(){
+//   //   play = true ;
+//   // }, 1000);
+// }
 
 function draw() {
+
+  // Rules , Title , Text and other utilities (Need to add more things as possible)
   background(30);
-  textSize(16);
+  textFont('Times New Roman');
+  textSize(32);
   textAlign(CENTER);
   stroke(175);
   fill(255);
-  let s = 'Welcome to 3D-Tictactoe shut up and play' ;
-  text(s , 1000 , 70 , 600 , 800);
+  let s = 'Nested TicTacToe' ;
+  text(s , 800 , 20 , 600 , 800);
+
   drawboard(Newboard);
 }
 
